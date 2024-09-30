e7b614480996db445f6742dde6f222b16469c45f
Yuvraj-Lolage <yuvilolage7@gmail.com>
2024-09-30 12:28:52 +0530

    changed port number

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = 4000;
+const port = 5050;
 const { ProjectRouter } = require('./routes/projects');
 
 //Database Connection
