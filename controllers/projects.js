473749b5fee5589b1824b147ea4a5a0a8c9c5ebf
Yuvraj-Lolage <yuvilolage7@gmail.com>
2024-10-07 14:15:04 +0530

    API for get product by categories

--- controllers/projects.js
+++ controllers/projects.js
@@ -3,10 +3,10 @@ const { ProjectModel } = require('../models/project_model')
 const getAllProjects = (req,res) => {
     try {
         const project = ProjectModel.find({}).then((result)=>{
-            res.send(result);
+            return res.send(result);
         })
     } catch (error) {
-        
+        return res.send(500);   
     }
 }
 
@@ -14,15 +14,27 @@ const getProjectById = (req,res) => {
     try {
         const projectId = req.params.id;
         const project = ProjectModel.find({ _id: projectId }).then((result)=>{
-            res.send(result);
+            return res.send(result);
         })
     } catch (error) {
-        
+        return res.send(500);  
+    }
+}
+
+const getProjectByCategory = (req, res) => {
+    try {
+        const projCategory = req.params.category;
+        const project = ProjectModel.find({ category: projCategory }).then((result)=>{
+            return res.send(result);
+        })
+    } catch (error) {
+        return res.send(500);  
     }
 }
 
 module.exports = {
     getAllProjects,
-    getProjectById
+    getProjectById,
+    getProjectByCategory
 }
 
--- routes/projects.js
+++ routes/projects.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const ProjectRouter = express.Router();
-const { getAllProjects, getProjectById } = require('../controllers/projects');
+const { getAllProjects, getProjectById, getProjectByCategory } = require('../controllers/projects');
 
 ProjectRouter.get('/', getAllProjects)
 
 ProjectRouter.get('/getProject/:id', getProjectById)
+
+ProjectRouter.get('/projects/:category', getProjectByCategory)
 
 module.exports = {
     ProjectRouter
